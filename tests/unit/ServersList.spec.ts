ffcf454630cd3698f50e9ec92c47741434741774
Oliver Brandon Bravo Cedillo <oliver.bravoc@gmail.com>
2021-05-28 11:27:14 -0500

    feat: global store added

--- tests/unit/ServersList.spec.ts
+++ tests/unit/ServersList.spec.ts
@@ -4,11 +4,7 @@ import store from '../../src/store/index';
 
 describe('ServersList component testing', () => {
   test('Servers list render correctyl with 0 elements', () => {
-    const wrapper = mount(ServersList, {
-      global: {
-        plugins: [store],
-      },
-    });
+    const wrapper = mount(ServersList, {global:{plugins:[store]}});
     const navigationItems = wrapper.findAll('[data-testid="single-server"]');
     expect(navigationItems.length).toEqual(0);
   });
